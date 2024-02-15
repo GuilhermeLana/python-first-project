4b34f78e95ec9174d29596a74272b2a3d6a0c2f0
Guilherme Lana <guilherme.lana@paytrack.com.br>
2024-02-15 13:46:04 -0300

    Trabalhando com listas

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 import os
 
+restaurantes = ['Dom corleone', 'Sushi garden']
+
 def exibir_nome_do_programa():
     print("""
 ░██████╗░█████╗░██████╗░░█████╗░██████╗░  ███████╗██╗░░██╗██████╗░██████╗░███████╗░██████╗░██████╗
@@ -20,15 +22,35 @@ def finalizar_app():
     os.system('cls')
     print('Finalizando o app\n')
 
+def retornar_menu_inicial():
+    input('\nDigite uma tecla para voltar ao menu principal')
+    main()
+
+def cadastrar_novo_restaurante():
+    os.system('cls')
+    print('Cadastro de novos restaurantes\n')
+    nome_do_restaurante = input('Digite o nome do restaurante: ')
+    restaurantes.append(nome_do_restaurante)
+    print(f'O restaurante {nome_do_restaurante} foi cadastrado com sucesso!')
+    retornar_menu_inicial() 
+
+def listar_restaurantes():
+    os.system('cls')
+    print('Listando Restaurantes\n')
+    for restaurante in restaurantes:
+        print(f'.{restaurante}')
+    retornar_menu_inicial()
+
+
 def escolher_opcao():
     try:
         opcao_escolhida = int(input('Escolha uma opção: '))
 
         match opcao_escolhida:
             case 1:
-                print('Cadastrar restaurante')
+                cadastrar_novo_restaurante()
             case 2:
-                print('Listar restaurantes')
+                listar_restaurantes()
             case 3:
                 print('Ativar restaurante')
             case 4:
