b0d93380ca4140700f807fad8627752d2ede6c43
Guilherme Lana <guilherme.lana@paytrack.com.br>
2024-02-15 11:54:11 -0300

    Adicionando try/except

--- app.py
+++ app.py
@@ -21,21 +21,30 @@ def finalizar_app():
     print('Finalizando o app\n')
 
 def escolher_opcao():
-    opcao_escolhida = int(input('Escolha uma opção: '))
+    try:
+        opcao_escolhida = int(input('Escolha uma opção: '))
 
-    match opcao_escolhida:
-        case 1:
-            print('Cadastrar restaurante')
-        case 2:
-            print('Listar restaurantes')
-        case 3:
-            print('Ativar restaurante')
-        case 4:
-            finalizar_app()
-        case _:
-            print('Opção inválida!')
+        match opcao_escolhida:
+            case 1:
+                print('Cadastrar restaurante')
+            case 2:
+                print('Listar restaurantes')
+            case 3:
+                print('Ativar restaurante')
+            case 4:
+                finalizar_app()
+            case _:
+                opcao_invalida()
+    except:
+        opcao_invalida()
+
+def opcao_invalida():
+    print('Opção inválida!\n')
+    input('Digite uma tecla para voltar ao menu principal')
+    main()
 
 def main():
+    os.system('cls')
     exibir_nome_do_programa()
     exibir_opcoes()
     escolher_opcao()
